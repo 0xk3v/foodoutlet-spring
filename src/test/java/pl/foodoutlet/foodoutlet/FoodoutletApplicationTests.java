e25d7fde7c13481b2458c1508be1f313feb069f1
n-kev1n <kevin_@tuta.io>
2024-01-21 14:57:59 +0100

    initial commit

---
+++ src/test/java/pl/foodoutlet/foodoutlet/FoodoutletApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.foodoutlet.foodoutlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodoutletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
