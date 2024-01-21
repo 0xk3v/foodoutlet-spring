de1cc79092e10b1a7599645d19d39157fbde9258
n-kev1n <kevin_@tuta.io>
2024-01-21 22:32:14 +0100

    added foodoutlet repository

--- src/main/java/pl/foodoutlet/foodoutlet/repository/FoodOutletRepo.java
+++ src/main/java/pl/foodoutlet/foodoutlet/repository/FoodOutletRepo.java
@@ -1,9 +1,8 @@
 package pl.foodoutlet.foodoutlet.repository;
 
-import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import pl.foodoutlet.foodoutlet.model.FoodOutlet;
 
-public interface FoodOutletRepo extends CrudRepository<FoodOutlet, Integer> {
+public interface FoodOutletRepo extends JpaRepository<FoodOutlet, Integer> {
 
 }
