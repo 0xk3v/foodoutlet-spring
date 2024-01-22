643cf57fcbcef0b85f7349b2ebfecf31edb99b8b
n-kev1n <kevin_@tuta.io>
2024-01-22 01:04:01 +0100

    changed id datatype

--- src/main/java/pl/foodoutlet/foodoutlet/repository/FoodOutletRepo.java
+++ src/main/java/pl/foodoutlet/foodoutlet/repository/FoodOutletRepo.java
@@ -3,6 +3,6 @@ package pl.foodoutlet.foodoutlet.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.foodoutlet.foodoutlet.model.FoodOutlet;
 
-public interface FoodOutletRepo extends JpaRepository<FoodOutlet, Integer> {
+public interface FoodOutletRepo extends JpaRepository<FoodOutlet, Long> {
 
 }
