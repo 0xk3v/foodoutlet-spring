d4f88f1762438909a96ccea57829853f8dc12c89
0xk3v <kevin_@tuta.io>
2024-01-22 19:12:02 +0100

    improved JDoc and also organized imports

--- src/main/java/pl/foodoutlet/foodoutlet/controller/RatingController.java
+++ src/main/java/pl/foodoutlet/foodoutlet/controller/RatingController.java
@@ -3,11 +3,25 @@ package pl.foodoutlet.foodoutlet.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import pl.foodoutlet.foodoutlet.model.Rating;
 import pl.foodoutlet.foodoutlet.schema.RatingSchema;
 import pl.foodoutlet.foodoutlet.service.RatingService;
+
+/**
+ * SpringBoot Controller to Manage Rating
+ * 
+ * @author Kévin
+ *
+ */
 
 @RestController
 @RequestMapping("/api/ratings")
@@ -20,18 +34,37 @@ public class RatingController {
      * Calls Rating Service to create a new Rating
      * 
      * @param ratingRequest
-     * @return A created user with type Rating
+     * @return A created Rating
      * @author Kévin
+     *
      */
+
     @PostMapping
     public Rating createRating(@RequestBody RatingSchema ratingRequest) {
         return ratingService.createRating(ratingRequest.getRating(), ratingRequest.getFoodOutletId());
     }
 
+    /**
+     * Calls Rating Service to Fetch all Ratings from the Database
+     * 
+     * @return A List of all Ratings
+     * @author Kévin
+     *
+     */
+
     @GetMapping
     public List<Rating> getAllRatings() {
         return ratingService.getAllRatings();
     }
+
+    /**
+     * Calls Rating Service to Fetch all Ratings from the Database
+     * 
+     * @param id
+     * @return A Single Rating based on provided param
+     * @author Kévin
+     * 
+     */
 
     @GetMapping("/{id}")
     public Rating getRatingById(@PathVariable Long id) {
