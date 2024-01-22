788ce338a99199f999dc7a73dacbe1bda13999a0
n-kev1n <kevin_@tuta.io>
2024-01-22 01:04:22 +0100

    added repository for rating model

---
+++ src/main/java/pl/foodoutlet/foodoutlet/repository/RatingRepo.java
@@ -0,0 +1,8 @@
+package pl.foodoutlet.foodoutlet.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.foodoutlet.foodoutlet.model.Rating;
+
+public interface RatingRepo extends JpaRepository<Rating, Long> {
+
+}
