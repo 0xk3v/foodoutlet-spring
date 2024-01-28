0592577465c05e58f0035a3ca6e48f1c35e0da05
0xk3v <kevin_@tuta.io>
2024-01-28 17:08:28 +0100

    added documentation on OutletController

--- src/main/java/pl/foodoutlet/foodoutlet/controller/OutletController.java
+++ src/main/java/pl/foodoutlet/foodoutlet/controller/OutletController.java
@@ -16,7 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.foodoutlet.foodoutlet.model.FoodOutlet;
 import pl.foodoutlet.foodoutlet.service.OutletService;
 
-@CrossOrigin(origins = "http://localhost:5173/")
+/**
+ * Controller class for managing Food Outlets in the Food Outlet Rating System.
+ * Exposes RESTful endpoints for CRUD operations on Food Outlets.
+ *
+ * @author Kévin
+ * @version 1.0
+ * 
+ */
+@CrossOrigin(origins = "http://localhost:5173/") // Allowing cross-origin requests from http://localhost:5173/
 @RestController
 @RequestMapping("/api/outlets")
 public class OutletController {
@@ -24,26 +32,65 @@ public class OutletController {
     @Autowired
     private OutletService outletService;
 
+    /**
+     * Endpoint for creating a new Food Outlet.
+     *
+     * @author Kévin
+     * @param outlet The Food Outlet object to be created.
+     * @return The created Food Outlet.
+     * 
+     */
     @PostMapping
     public FoodOutlet createFoodOutlet(@RequestBody FoodOutlet outlet) {
         return outletService.createOutlet(outlet);
     }
 
+    /**
+     * Endpoint for retrieving a list of all Food Outlets.
+     *
+     * @author Kévin
+     * @return List of Food Outlets.
+     * 
+     */
     @GetMapping
     public List<FoodOutlet> getAllOutlets() {
         return outletService.getAllOutlets();
     }
 
+    /**
+     * Endpoint for retrieving a specific Food Outlet by its ID.
+     *
+     * @author Kévin
+     * @param id The ID of the Food Outlet to retrieve.
+     * @return The requested Food Outlet.
+     * 
+     */
     @GetMapping("/{id}")
     public FoodOutlet getOutletById(@PathVariable Long id) {
         return outletService.getOutletById(id);
     }
 
+    /**
+     * Endpoint for updating an existing Food Outlet.
+     * 
+     * @author Kévin
+     * @param id     The ID of the Food Outlet to update.
+     * @param outlet The updated Food Outlet data.
+     * @return The updated Food Outlet.
+     * 
+     */
     @PutMapping("/{id}")
     public FoodOutlet updateOutlet(@PathVariable Long id, @RequestBody FoodOutlet outlet) {
         return outletService.updateOutlet(id, outlet);
     }
 
+    /**
+     * Endpoint for deleting a Food Outlet by its ID.
+     *
+     * @author Kévin
+     * @param id The ID of the Food Outlet to delete.
+     * 
+     */
     @DeleteMapping("/{id}")
     public void deleteOutlet(@PathVariable Long id) {
         outletService.deleteOutlet(id);
