77e353ba1c9598ea49224925964557a2f6328101
0xk3v <kevin_@tuta.io>
2024-01-28 15:06:11 +0100

    renamed couple classes

--- src/main/java/pl/foodoutlet/foodoutlet/repository/OutletRepo.java
+++ src/main/java/pl/foodoutlet/foodoutlet/repository/OutletRepo.java
@@ -3,6 +3,6 @@ package pl.foodoutlet.foodoutlet.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.foodoutlet.foodoutlet.model.FoodOutlet;
 
-public interface FoodOutletRepo extends JpaRepository<FoodOutlet, Long> {
+public interface OutletRepo extends JpaRepository<FoodOutlet, Long> {
 
 }
