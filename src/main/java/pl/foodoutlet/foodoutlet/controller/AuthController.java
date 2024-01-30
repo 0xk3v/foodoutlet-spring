dd4ced5f8a24a37ed0a77078814a98048bbaa628
0xk3v <kevin_@tuta.io>
2024-01-30 11:53:49 +0100

    added documentation

--- src/main/java/pl/foodoutlet/foodoutlet/controller/AuthController.java
+++ src/main/java/pl/foodoutlet/foodoutlet/controller/AuthController.java
@@ -10,6 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.foodoutlet.foodoutlet.model.User;
 import pl.foodoutlet.foodoutlet.service.AuthService;
 
+/**
+ * The AuthController class handles authentication-related HTTP requests.
+ * It provides endpoints for user sign-up and sign-in operations.
+ *
+ * @author Raymond
+ * @version 1.0
+ * 
+ */
 @RestController
 @CrossOrigin(origins = "http://localhost:5173/")
 @RequestMapping("/api/auth")
@@ -18,9 +26,28 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    /**
+     * Handles the HTTP POST request for user sign-up.
+     *
+     * @author Raymond
+     * @param user The User object containing sign-up information.
+     * @return The User object representing the signed-up user.
+     */
     @PostMapping("/signup")
     public User signUp(@RequestBody User user) {
         return authService.signUp(user);
     }
 
+    /**
+     * Handles the HTTP POST request for user sign-in.
+     * 
+     * @author Raymond
+     * @param user The User object containing sign-in information.
+     * @return The User object representing the signed-in user.
+     * 
+     */
+    @PostMapping("/signin")
+    public User signIn(@RequestBody User user) {
+        return authService.signIn(user);
+    }
 }
