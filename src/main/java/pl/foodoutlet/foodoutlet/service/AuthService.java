255daacd5a8b5f42b4b848ab6a86172e9fb9d016
0xk3v <kevin_@tuta.io>
2024-01-30 11:54:19 +0100

    added documentation

--- src/main/java/pl/foodoutlet/foodoutlet/service/AuthService.java
+++ src/main/java/pl/foodoutlet/foodoutlet/service/AuthService.java
@@ -7,23 +7,63 @@ import org.springframework.stereotype.Service;
 import pl.foodoutlet.foodoutlet.model.User;
 import pl.foodoutlet.foodoutlet.repository.UserRepo;
 
+/**
+ * The AuthService class provides authentication-related services,
+ * including user sign-up and sign-in operations.
+ *
+ * @author Raymond
+ * @version 1.0
+ */
 @Service
 public class AuthService {
 
+    /**
+     * The repository for accessing User entities.
+     */
     @Autowired
     public UserRepo userRepo;
 
+    /**
+     * The Argon2PasswordEncoder used for hashing passwords.
+     * It is configured with specific parameters for hashing.
+     */
     private Argon2PasswordEncoder passwordEncoder = new Argon2PasswordEncoder(32, 64, 1, 15 * 1024, 2);
 
+    /**
+     * Signs up a new user by encoding the password and saving the user to the
+     * repository.
+     *
+     * @param user The User object representing the user to be signed up.
+     * @return The User object representing the signed-up user.
+     */
     public User signUp(User user) {
-
         String unHashedPassword = user.getPassword();
         user.setPassword(passwordEncoder.encode(unHashedPassword));
         return userRepo.save(user);
     }
 
-    // public User signIn(User user){
-    // return userRepo.findOne(user);
-    // }
+    /**
+     * Signs in a user by verifying the provided credentials against the stored
+     * data.
+     *
+     * @param user The User object representing the user attempting to sign in.
+     * @return The User object representing the signed-in user if successful, or
+     *         null otherwise.
+     */
+    public User signIn(User user) {
+        try {
+            User signingUser = userRepo.findByUsername(user.getUsername());
+            boolean passwordVerify = passwordEncoder.matches(user.getPassword(), signingUser.getPassword());
+
+            if (passwordVerify) {
+                return signingUser;
+            } else {
+                return null;
+            }
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
 }
--- src/main/java/pl/foodoutlet/foodoutlet/service/OutletService.java
+++ src/main/java/pl/foodoutlet/foodoutlet/service/OutletService.java
@@ -3,6 +3,7 @@ package pl.foodoutlet.foodoutlet.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import pl.foodoutlet.foodoutlet.model.FoodOutlet;
@@ -12,7 +13,7 @@ import pl.foodoutlet.foodoutlet.repository.OutletRepo;
  * Service class for managing Food Outlets in the Food Outlet Rating System.
  * Provides business logic for CRUD operations on Food Outlets.
  *
- * @author Kevin
+ * @author Raymond
  * @version 1.0
  * 
  */
@@ -25,7 +26,7 @@ public class OutletService {
     /**
      * Creates a new Food Outlet.
      *
-     * @author Kévin
+     * @author Raymond
      * @param outlet The Food Outlet object to be created.
      * @return The created Food Outlet.
      * 
@@ -37,18 +38,18 @@ public class OutletService {
     /**
      * Retrieves a list of all Food Outlets.
      *
-     * @author Kévin
+     * @author Raymond
      * @return List of Food Outlets.
      * 
      */
     public List<FoodOutlet> getAllOutlets() {
-        return outletRepo.findAll();
+        return outletRepo.findAll(Sort.by(Sort.Direction.DESC, "id"));
     }
 
     /**
      * Retrieves a specific Food Outlet by its ID.
      *
-     * @author Kévin
+     * @author Raymond
      * @param id The ID of the Food Outlet to retrieve.
      * @return The requested Food Outlet, or null if not found.
      * 
@@ -60,7 +61,7 @@ public class OutletService {
     /**
      * Updates an existing Food Outlet.
      *
-     * @author Kévin
+     * @author Raymond
      * @param id     The ID of the Food Outlet to update.
      * @param outlet The updated Food Outlet data.
      * @return The updated Food Outlet if successful, or null if the outlet doesn't
@@ -78,7 +79,7 @@ public class OutletService {
     /**
      * Deletes a Food Outlet by its ID.
      *
-     * @author Kévin
+     * @author Raymond
      * @param id The ID of the Food Outlet to delete.
      * 
      */
